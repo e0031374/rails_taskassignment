a6e2c4ed33444c6560b4aa500bee6b8525d30593
owo <e0031374@u.nus.edu>
2019-12-27 15:57:06 +0800

    FORGOT: now notes and labels are being fetched from server upon mounting

---
+++ frontend/src/constants.js
@@ -0,0 +1,22 @@
+//const hostname = window.location.hostname === "localhost" 
+//    ? window.location.hostname + ":3001"
+//    : "api." + window.location.hostname; 
+//const ROOT_API = window.location.protocol + "//" + hostname;
+
+// dev only, we falling back on the proxy
+const ROOT_API = "";
+
+export const API_URL = {
+    TASK_BASE: `${ROOT_API}/api/v1/tasks`,
+    LABEL_BASE: `${ROOT_API}/api/v1/labels`,
+}
+
+export const TASK_BASE = "TASK_BASE";
+export const LABEL_BASE = "LABEL_BASE";
+
+
+
+
+export const BASE_COLOUR = "#ff6699";
+export const TRIM_COLOUR = "#ff0066";
+export const HIGHLIGHT_COLOUR = "#fbf9f9";
