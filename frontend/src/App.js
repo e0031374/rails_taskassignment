16d2938e015e5c40e28164975d8d1c93c5dc9618
owo <e0031374@u.nus.edu>
2020-01-11 05:13:37 +0800

    FIX: removed serviceworker and logo.svg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import StageOne from './components/StageOne';
 
@@ -7,31 +6,10 @@ import StageOne from './components/StageOne';
 
 function App() {
   return (
-    <div >
+    <div>
         <StageOne />
     </div>
   );
 }
 
 export default App;
-//function App() {
-//  return (
-//    <div className="App">
-//      <header className="App-header">
-//        <img src={logo} className="App-logo" alt="logo" />
-//        <p>
-//          Edit <code>src/App.js</code> and save to reload.
-//        </p>
-//        <a
-//          className="App-link"
-//          href="https://reactjs.org"
-//          target="_blank"
-//          rel="noopener noreferrer"
-//        >
-//          Learn React
-//        </a>
-//        <NoteLayout />
-//      </header>
-//    </div>
-//  );
-//}
